13a055292f6033037cedc5c61e437a65492d5d3b
Catalin Dumitru <dumitru.catalin96@yahoo.com>
2024-07-12 09:59:21 +0300

    Add initial boilerplate. Integrate Metamask SDK and enable connect and sign.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# web3-dapp-samples
-A repo with multiple smart contracts integrations
+## Introduction
+
+This is a Web3 DApp sample implementing multiple smart contracts in React + Next.js.
+
+## How It Works
+
+## Demo
+
+To be updated
+
+## Deploy Your Own
+
+To be updated
+
+## Getting Started
+
+First, install the dependencies:
+
+```bash
+npm install
+# or
+yarn
+# or
+pnpm install
+```
+
+Then, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ lib/utils.ts
@@ -0,0 +1,31 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const formatBalance = (rawBalance: string) => {
+  const balance = (parseInt(rawBalance) / 1000000000000000000).toFixed(2);
+  return balance;
+};
+
+export const formatChainAsNum = (chainIdHex: string) => {
+  const chainIdNum = parseInt(chainIdHex);
+  return chainIdNum;
+};
+
+export const formatAddress = (addr: string | undefined) => {
+  return `${addr?.substring(0, 8)}...`;
+};
+
+export const getNetworkRPC = (network: string) => {
+  switch (network) {
+    case "localhost":
+      return "http://127.0.0.1:8545/";
+    case "linea-testnet":
+      return `${process.env.LINEA_TEST_RPC_ENDPOINT}/${process.env.INFURA_API_KEY}`;
+    default:
+      throw new Error(`Unsupported network: ${network}`);
+  }
+};
